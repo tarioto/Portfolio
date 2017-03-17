9ae9567eadf829e80e4e6080578419b9937cbd2d
Tim Arioto <timarioto@gmail.com>
2017-03-16 21:46:16 -0700

    Remove button component from homepage

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -12,20 +12,13 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
-import Button from '../../components/Button';
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <div className="container">
-        <div className="jumbotron">
-          <h1>
-            <FormattedMessage {...messages.header} />
-          </h1>
-          <Button />
-          <button className="btn btn-primary btn-lg">test</button>
-        </div>
-      </div>
+      <h1>
+        <FormattedMessage {...messages.header} />
+      </h1>
     );
   }
 }
