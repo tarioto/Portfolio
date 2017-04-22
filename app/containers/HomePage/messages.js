da70c8ee4f210363c185a9f71e0c83cc55a01455
Tim Arioto <timarioto@gmail.com>
2017-04-21 21:57:41 -0700

    Add more to About

--- app/containers/HomePage/messages.js
+++ app/containers/HomePage/messages.js
@@ -8,6 +8,6 @@ import { defineMessages } from 'react-intl';
 export default defineMessages({
   header: {
     id: 'app.components.HomePage.header',
-    defaultMessage: 'I am a Full-stack Javascript Engineer who likes to work in React.',
+    defaultMessage: 'I am a Full-stack Javascript Engineer who likes to work in React. I have experiance with multiple framworks including Angular and Backbone.',
   },
 });
