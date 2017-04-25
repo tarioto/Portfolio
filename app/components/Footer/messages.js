1d28b0b58b0dca8aeb81afc182a53686e09b211b
Tim Arioto <timarioto@gmail.com>
2017-04-24 22:57:31 -0700

    Add name to footer message

--- app/components/Footer/messages.js
+++ app/components/Footer/messages.js
@@ -8,6 +8,6 @@ import { defineMessages } from 'react-intl';
 export default defineMessages({
   header: {
     id: 'app.components.Footer.header',
-    defaultMessage: 'This is the Footer component !',
+    defaultMessage: 'made by Tim Arioto',
   },
 });
