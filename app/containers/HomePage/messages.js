55dbc00b87e28147fd1a32b056c3cce203321b73
Tim Arioto <timarioto@gmail.com>
2017-05-19 15:01:43 -0700

    Add name and title for formated messages

--- app/containers/HomePage/messages.js
+++ app/containers/HomePage/messages.js
@@ -10,4 +10,12 @@ export default defineMessages({
     id: 'app.components.HomePage.header',
     defaultMessage: 'I am a Full-stack Javascript Engineer who likes to work in React. I have experiance with multiple framworks including Angular and Backbone.',
   },
+  name: {
+    id: 'app.components.HomePage.name',
+    defaultMessage: 'Tim Arioto',
+  },
+  title: {
+    id: 'app.components.HomePage.title',
+    defaultMessage: 'Full-stack JS Engineer',
+  },
 });
