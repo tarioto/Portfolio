a7374ddbbdcb6d714e6d716444eeb860b3353b9f
Tim Arioto <timarioto@gmail.com>
2017-05-22 11:55:58 -0700

    Add Social container

---
+++ app/containers/Social/messages.js
@@ -0,0 +1,13 @@
+/*
+ * Social Messages
+ *
+ * This contains all the text for the Social component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  header: {
+    id: 'app.containers.Social.header',
+    defaultMessage: 'This is Social container !',
+  },
+});
