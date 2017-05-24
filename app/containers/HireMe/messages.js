db2ca778b63162bb45fbe4a23ed34fa4d556abb7
Tim Arioto <timarioto@gmail.com>
2017-05-24 14:14:59 -0700

    Add Hire me message

--- app/containers/HireMe/messages.js
+++ app/containers/HireMe/messages.js
@@ -6,8 +6,12 @@
 import { defineMessages } from 'react-intl';
 
 export default defineMessages({
-  header: {
-    id: 'app.containers.HireMe.header',
-    defaultMessage: 'This is HireMe container !',
+  hireme: {
+    id: 'app.containers.HireMe.hireme',
+    defaultMessage: 'Want to hire me?!',
+  },
+  callOrEmail: {
+    id: 'app.containers.HireMe.callOrEmail',
+    defaultMessage: 'Call me at 925.989.2339 or email me at timarioto@gmail.com',
   },
 });
