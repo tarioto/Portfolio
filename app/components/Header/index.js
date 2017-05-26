4f77295b5eeee8c54a3c57c92d596209c84ee2c9
Tim Arioto <timarioto@gmail.com>
2017-05-25 20:58:13 -0700

    Cleaned up

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -19,9 +19,6 @@ function Header() {
       <div className="navbar-header">
         <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse" aria-expanded="false">
           <span className="sr-only">Toggle navigation</span>
-          {/* <span className="icon-bar"></span>
-          <span className="icon-bar"></span>
-          <span className="icon-bar"></span> */}
           <i className="fa fa-bars" aria-hidden="true"></i>
         </button>
         <span className="navbar-brand">
@@ -31,13 +28,6 @@ function Header() {
       </div>
       <div className="navbar-collapse collapse">
         <ul className="nav navbar-nav nav-pills">
-          {/* <li><HeaderLink to={'/'}><a><i className="fa fa-home" aria-hidden="true" /> Home</a></HeaderLink></li>
-          <li><HeaderLink to={'about'}><a><i className="fa fa-user" aria-hidden="true" /> About</a></HeaderLink></li>
-          <li><HeaderLink to={'projects'}><a><i className="fa fa-briefcase" aria-hidden="true" /> Projects</a></HeaderLink></li>
-          <li><HeaderLink to={'hireme'}><a><i className="fa fa-commenting" aria-hidden="true" /> Hire Me</a></HeaderLink></li>
-          <li><HeaderLink to={'social'}><a><i className="fa fa-thumbs-up" aria-hidden="true" /> Social</a></HeaderLink></li> */}
-
-
           <li><Link to={'/'}><a><i className="fa fa-home" aria-hidden="true" /> Home</a></Link></li>
           <li><Link to={'about'}><a><i className="fa fa-user" aria-hidden="true" /> About</a></Link></li>
           <li><Link to={'projects'}><a><i className="fa fa-briefcase" aria-hidden="true" /> Projects</a></Link></li>
