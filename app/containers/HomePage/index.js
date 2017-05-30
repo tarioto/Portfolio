69a8b6d43615890d05118aaab055f0103f7b5ad5
Tim Arioto <timarioto@gmail.com>
2017-05-29 19:47:48 -0700

    Remove card on home page

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -27,7 +27,7 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
             </div>
           </div>
         </div>
-        <div className="row">
+        {/* <div className="row">
           <div className="col-xs-12">
             <div className="panel panel-primary">
               <div className="panel-body">
@@ -35,7 +35,7 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     );
   }
